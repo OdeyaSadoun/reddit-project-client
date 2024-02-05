538b908f2effc17967a4a8ee667ca5b3bbb88a81
אודיה סעדון <osadoun@g.jct.ac.il>
2024-02-05 09:10:44 +0200

    put the subreddit interface in seperate file

---
+++ frontend/src/interfaces/Subreddit.interface.ts
@@ -0,0 +1,9 @@
+export interface Subreddit {
+    title: string;
+    selftext: string;
+    upvote_ratio: number;
+    ups: number;
+    downs: number;
+    score: number;
+    sentiment: string;
+  }
