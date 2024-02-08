b259f67f68e3d0088572a1d3db40920853c984a6
אודיה סעדון <osadoun@g.jct.ac.il>
2024-02-08 09:34:17 +0200

    refactoring checkhour

--- frontend/src/static_comps/CheckHour.tsx
+++ frontend/src/static_comps/CheckHour.tsx
@@ -1,19 +1,19 @@
-export function getGreeting() {
-    const now = new Date();
-    const hour = now.getHours();
-  
-    if (hour >= 5 && hour < 12) {
-      return "Good morning, ";
-    } else if (hour >= 12 && hour < 18) {
-      return "Good afternoon,";
-    } else if (hour >= 18 && hour < 24) {
-      return "Good evening,";
+export function getGreeting(): string {
+  const now: Date = new Date();
+  const hour: number = now.getHours();
+
+  if (hour >= 5 && hour < 12) {
+    return "Good morning, ";
+  } else if (hour >= 12 && hour < 18) {
+    return "Good afternoon,";
+  } else if (hour >= 18 && hour < 24) {
+    return "Good evening,";
+  } else {
+    const isFriday: boolean = now.getDay() === 5;
+    if (isFriday && hour < 18) {
+      return "Almost Shabbat Shalom,";
     } else {
-      const isFriday = now.getDay() === 6;
-      if (isFriday && hour < 18) {
-        return "Almost Shabbat Shalom,";
-      } else {
-        return "Good night,";
-      }
+      return "Good night,";
     }
+  }
 }
