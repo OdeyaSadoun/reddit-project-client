50eeb35cfb7eab6757d9df3d4d261cb350bac44d
אודיה סעדון <osadoun@g.jct.ac.il>
2024-02-08 09:35:34 +0200

    refactoring loading

--- frontend/src/static_comps/Loading.tsx
+++ frontend/src/static_comps/Loading.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import { BeatLoader } from "react-spinners";
 
-const Loading = () => {
+const Loading: React.FC = () => {
   return (
     <div className="text-center d-flex justify-content-center">
-      {/* <h2 className="lead" >Loading</h2> */}
       <div className="m-1">
         <BeatLoader color="#36d7b7" />
       </div>
