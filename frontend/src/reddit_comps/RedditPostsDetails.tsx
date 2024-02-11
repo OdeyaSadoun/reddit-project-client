5971f5451ea855ce63ad841aadc79690c9a7f726
אודיה סעדון <osadoun@g.jct.ac.il>
2024-02-11 17:02:32 +0200

    refactor redit posts details

--- frontend/src/reddit_comps/RedditPostsDetails.tsx
+++ frontend/src/reddit_comps/RedditPostsDetails.tsx
@@ -1,38 +1,48 @@
-// RedditPostDetails.tsx
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 
-import { useParams } from "react-router-dom";
-import { Subreddit } from "src/interfaces/Subreddit.interface";
 import RedditItem from "./RedditItem";
 
-const RedditPostDetails: React.FC = () => {
+import { useParams } from "react-router-dom";
+import { Subreddit } from "src/interfaces/Subreddit.interface";
+import { getToken } from "src/static_comps/GetToken";
 
+const RedditPostDetails: React.FC = () => {
   const [posts, setPosts] = useState<Subreddit[]>([]);
 
   const { reddit_id } = useParams<{ reddit_id: string }>();
 
   const API_URL: string = "http://localhost:8000";
 
-  useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const token = localStorage.getItem("access_token");
+  const getPostsByHistorySearch = async (): Promise<
+    Subreddit[] | undefined
+  > => {
+    try {
+      const token: string = getToken();
 
-        if (!token) {
-          throw new Error("User token is not available");
+      const postsByHistorySearch = await axios.get<Subreddit[]>(
+        `${API_URL}/reddits/history/${reddit_id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
         }
-        const response = await axios.get(
-          `${API_URL}/reddits/history/${reddit_id}`,
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-              "Content-Type": "application/json",
-            },
-          }
-        );
+      );
 
-        setPosts(response.data);
+      return postsByHistorySearch.data;
+    } catch (error) {
+      console.error("Error getting posts:", error);
+    }
+  };
+
+  useEffect(() => {
+    const fetchPosts = async (): Promise<void> => {
+      try {
+        const postsByHistorySearch = await getPostsByHistorySearch();
+        if (postsByHistorySearch) {
+          setPosts(postsByHistorySearch);
+        }
       } catch (error) {
         console.error("Error fetching posts:", error);
       }
