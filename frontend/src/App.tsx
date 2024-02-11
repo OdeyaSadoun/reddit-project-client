35af4ab44fb778766ec34e790d16530df85446d8
אודיה סעדון <osadoun@g.jct.ac.il>
2024-02-11 17:10:18 +0200

    more refactoring

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,17 @@ import { Routes, Route } from "react-router-dom";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import { AppContext, ContextType } from "./context/Context";
+
 import userSlice from "./features/userSlice";
-import Home from "./static_comps/Home";
-import Login from "./auth_comps/Login";
-import Register from "./auth_comps/Register";
-import RedditHome from "./reddit_comps/RedditHome";
+
 import Footer from "./static_comps/Footer";
 import Header from "./static_comps/Header";
+import Home from "./static_comps/Home";
 import HomeUser from "./user_comps/HomeUser";
+import Login from "./auth_comps/Login";
+import RedditHome from "./reddit_comps/RedditHome";
 import RedditPostDetails from "./reddit_comps/RedditPostsDetails";
+import Register from "./auth_comps/Register";
 
 const myStore = configureStore({
   reducer: {
@@ -20,6 +22,7 @@ const myStore = configureStore({
 });
 
 function App() {
+
   const [showInfo, setShowInfo] = useState(false);
 
   const closeUserIconDiv = () => {
@@ -39,7 +42,6 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/home/:username" element={<HomeUser />} />
-          {/* <Route path="/home/:username" element={<HomeUser />} /> */}
           <Route path="/reddit" element={<RedditHome />} />
           <Route path="/history/:reddit_id" element={<RedditPostDetails />} />
           <Route path="/" element={<Home />} />
