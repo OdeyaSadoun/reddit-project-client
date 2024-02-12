f2fac89c761fac4e0193fe243a992bf8fbf9170f
אודיה סעדון <osadoun@g.jct.ac.il>
2024-02-12 11:27:48 +0200

    add style to header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,6 @@ import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
-import { AppContext, ContextType } from "./context/Context";
-
-import userSlice from "./features/userSlice";
 
 import Footer from "./static_comps/Footer";
 import Header from "./static_comps/Header";
@@ -14,42 +11,29 @@ import Login from "./auth_comps/Login";
 import RedditHome from "./reddit_comps/RedditHome";
 import RedditPostDetails from "./reddit_comps/RedditPostsDetails";
 import Register from "./auth_comps/Register";
-
-const myStore = configureStore({
-  reducer: {
-    userSlice,
-  },
-});
-
+import RedditSearchHistory from "./reddit_comps/RedditsHistory";
 
 function App() {
-
   const [showInfo, setShowInfo] = useState(false);
 
   const closeUserIconDiv = () => {
     setShowInfo(!showInfo);
   };
 
-  const value: ContextType = {
-    showInfo: showInfo,
-    setShowInfo: setShowInfo,
-  };
-
   return (
-    <AppContext.Provider value={value}>
-      <Provider store={myStore}>
-        <Header /> 
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/home/:username" element={<HomeUser />} />
-          <Route path="/reddit" element={<RedditHome />} />
-          <Route path="/history/:reddit_id" element={<RedditPostDetails />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-        <Footer />
-      </Provider>
-    </AppContext.Provider>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home/:username" element={<HomeUser />} />
+        <Route path="/reddit" element={<RedditHome />} />
+        <Route path="/history/:reddit_id" element={<RedditPostDetails />} />
+        <Route path="/history" element={<RedditSearchHistory />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
--- frontend/src/static_comps/Header.tsx
+++ frontend/src/static_comps/Header.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "../style/Header.css";
 
-const Header = () => {
+const Header: React.FC = () => {
+
+  const nav = useNavigate();
+
+  const logOut = () => {
+    localStorage.removeItem("user");
+    localStorage.removeItem("access_token");
+    localStorage.removeItem("refresh_token");
+    nav("/");
+  };
+
   return (
-    <div>Header</div>
-  )
-}
+    <div className="main-header">
+      <header className="container-fluid shadow">
+        <div className="container">
+          <div className="row align-items-center justify-content-between">
+            <nav className="nav col-auto">  
+                <li>
+                  <Link to="/" className="py-1">
+                    <img src="logo.png" className="logo py-2"></img>
+                  </Link>
+                </li>
+            </nav>
+            <div className="col-auto user">
+              <i
+                className="fa fa-user p-1 mt-1 home-btn"
+                aria-hidden="true"
+              ></i>
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
