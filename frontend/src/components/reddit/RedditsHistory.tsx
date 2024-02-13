4ad9a3a5b0b577f5fd5d965c6425502d6fe8cdb8
אודיה סעדון <osadoun@g.jct.ac.il>
2024-02-13 17:37:11 +0200

    fix history, when is today show hour and not date

--- frontend/src/components/reddit/RedditsHistory.tsx
+++ frontend/src/components/reddit/RedditsHistory.tsx
@@ -6,10 +6,11 @@ import { format } from "date-fns";
 
 import { getToken } from "../static/GetToken";
 import { RedditSearch } from "../../interfaces/RedditSearch.interface";
+import { isToday } from "../static/CheckDay";
 
 const RedditSearchHistory: React.FC = () => {
   const [searches, setSearches] = useState<RedditSearch[]>([]);
-  
+
   const API_URL: string = "http://localhost:8000";
 
   const getHistorySearch = async (): Promise<RedditSearch[] | undefined> => {
@@ -49,8 +50,12 @@ const RedditSearchHistory: React.FC = () => {
     <div className="history-container container mx-auto margin-buttom-container">
       <h2 className="text-center display-6 py-3">Search History</h2>
       <div>
-        {searches.map((search) => (
-          <Link to={`/user/history/${search.id}`} key={search.id} className="link-decoration-none">
+        {searches.reverse().map((search) => (
+          <Link
+            to={`/user/history/${search.id}`}
+            key={search.id}
+            className="link-decoration-none"
+          >
             <div className="search-box ps-3 pt-4">
               <div className="search-info d-flex">
                 <p className="search-text px-2">{search.reddit}</p>
@@ -58,7 +63,9 @@ const RedditSearchHistory: React.FC = () => {
               </div>
               <div className="date-info pe-3">
                 <p className="date-text">
-                  {format(new Date(search.created_date), "dd/MM/yyyy")}
+                  {isToday(new Date(search.created_date))
+                    ? format(new Date(search.created_date), "HH:mm")
+                    : format(new Date(search.created_date), "dd/MM/yyyy")}
                 </p>
               </div>
             </div>
