2052430497726c3f51f9f28b15b43082bc5a75aa
אודיה סעדון <osadoun@g.jct.ac.il>
2024-02-13 09:28:39 +0200

    fix header before login

--- frontend/src/components/static/Header.tsx
+++ frontend/src/components/static/Header.tsx
@@ -2,7 +2,6 @@ import React, { useContext, useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 const Header: React.FC = () => {
-
   const nav = useNavigate();
 
   const logOut = () => {
@@ -16,20 +15,33 @@ const Header: React.FC = () => {
     <div className="main-header">
       <header className="container-fluid shadow">
         <div className="container">
-          <div className="row align-items-center justify-content-between">
-            <nav className="nav col-auto">  
-                <li>
-                  <Link to="/" className="py-1">
-                    <img src="logo.png" className="logo py-2"></img>
+          <div>
+            <nav className="row align-items-center justify-content-between">
+              <div className="col-auto">
+                <Link to="/" className="py-1">
+                  <img src="logo.png" className="logo py-2"></img>
+                </Link>
+              </div>
+              <div className="col-auto d-flex">
+                <div className="col-auto py-3 px-2 ">
+                  <Link
+                    to="/login"
+                    className="link-hover-color px-2 text-secondary link-offset-2 link-offset-3-hover link link-decoration-none link-hover-underline"
+                  >
+                    login
                   </Link>
-                </li>
+                  <Link
+                    to="/register"
+                    className="link-hover-color px-2 text-secondary link-offset-2 link-offset-3-hover link link-decoration-none link-hover-underline"
+                  >
+                    sign up
+                  </Link>
+                </div>
+                <div className="user col-auto p-2">
+                  <i className="fa fa-user home-btn" aria-hidden="true"></i>
+                </div>
+              </div>
             </nav>
-            <div className="col-auto user">
-              <i
-                className="fa fa-user p-1 mt-1 home-btn"
-                aria-hidden="true"
-              ></i>
-            </div>
           </div>
         </div>
       </header>
