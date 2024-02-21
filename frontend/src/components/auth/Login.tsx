e22671b1fe4822e3c8c1a4bd449f5718ce7e79cd
אודיה סעדון <osadoun@g.jct.ac.il>
2024-02-21 08:03:30 +0200

    seperate the logix and display in auth

--- frontend/src/components/auth/Login.tsx
+++ frontend/src/components/auth/Login.tsx
@@ -1,18 +1,6 @@
 import axios from "axios";
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-
-import { LockOutlined } from "@mui/icons-material";
-import {
-  Container,
-  CssBaseline,
-  Box,
-  Avatar,
-  Typography,
-  TextField,
-  Button,
-  Grid,
-} from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 import { Token } from "../../types/Token.type";
 import {
@@ -20,6 +8,8 @@ import {
   saveTokensToLocalStorage,
   saveUserToLocalStorage,
 } from "./TokenFuncs";
+
+import LoginDisplay from "../display/auth/LoginDisplay";
 
 const Login: React.FC = () => {
   const [email, setEmail] = useState<string>("");
@@ -69,80 +59,14 @@ const Login: React.FC = () => {
   };
 
   return (
-    <>
-      <Container maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            mt: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            p: 3, // Optional: Add padding if needed
-            boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
-            border: "2px solid #E5D6F2",
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "#F5B3D7" }}>
-            <LockOutlined />
-          </Avatar>
-          <Typography variant="h5">Login</Typography>
-          <Box sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoFocus
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="password"
-              name="password"
-              label="Password"
-              type="password"
-              value={password}
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-            />
-            {incorrectUserOrPassword && (
-              <Typography sx={{ color: "red", fontSize: 12 }}>
-                Incorrect email or password
-              </Typography>
-            )}
-            <Button
-              fullWidth
-              variant="contained"
-              sx={{
-                mt: 3,
-                mb: 2,
-                backgroundColor: "#EDC2F0",
-                "&:hover": {
-                  backgroundColor: "#EDC2F0",
-                },
-              }}
-              onClick={handleLogin}
-            >
-              Login
-            </Button>
-
-            <Grid container justifyContent={"center"}>
-              <Grid item>Don't have an account? </Grid>
-              <Grid item>
-                <Link to="/register">Register</Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-      </Container>
-    </>
+    <LoginDisplay
+      setEmail={setEmail}
+      email={email}
+      handleLogin={handleLogin}
+      password={password}
+      setPassword={setPassword}
+      incorrectUserOrPassword={incorrectUserOrPassword}
+    />
   );
 };
 
--- frontend/src/components/auth/Register.tsx
+++ frontend/src/components/auth/Register.tsx
@@ -15,16 +15,22 @@ import {
 } from "@mui/material";
 
 import { newUser } from "../../types/NewUser.type";
-import { getUserInfo, saveTokensToLocalStorage, saveUserToLocalStorage } from "./TokenFuncs";
+import {
+  getUserInfo,
+  saveTokensToLocalStorage,
+  saveUserToLocalStorage,
+} from "./TokenFuncs";
 import { Token } from "../../types/Token.type";
-
+import RegisterDisplay from "../display/auth/RegisterDisplay";
 
 const Register: React.FC = () => {
   const [email, setEmail] = useState<string>("");
   const [name, setName] = useState<string>("");
   const [password, setPassword] = useState<string>("");
-  const [incorrectEmailFormat, setIncorrectEmailFormat] = useState<boolean>(false);
-  const [incorrectPasswordFormat, setIncorrectPasswordFormat] = useState<boolean>(false);
+  const [incorrectEmailFormat, setIncorrectEmailFormat] =
+    useState<boolean>(false);
+  const [incorrectPasswordFormat, setIncorrectPasswordFormat] =
+    useState<boolean>(false);
   const [emailAlreadyRegistered, setEmailAlreadyRegistered] =
     useState<boolean>(false);
 
@@ -40,7 +46,8 @@ const Register: React.FC = () => {
           headers: {
             "Content-Type": "application/json",
           },
-        });
+        }
+      );
 
       return userTokenAfterRegister.data;
     } catch (error: any) {
@@ -55,8 +62,8 @@ const Register: React.FC = () => {
       };
     }
   };
-  
-  const handleRegister = async () : Promise<void>  => {
+
+  const handleRegister = async (): Promise<void> => {
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     if (!emailRegex.test(email)) {
       setIncorrectEmailFormat(true);
@@ -77,8 +84,13 @@ const Register: React.FC = () => {
 
     try {
       const userTokenAfterRegister = await addUser(newUser);
-      saveTokensToLocalStorage(userTokenAfterRegister.access_token, userTokenAfterRegister.refresh_token);
-      const currentUser = await getUserInfo(userTokenAfterRegister.access_token);
+      saveTokensToLocalStorage(
+        userTokenAfterRegister.access_token,
+        userTokenAfterRegister.refresh_token
+      );
+      const currentUser = await getUserInfo(
+        userTokenAfterRegister.access_token
+      );
       saveUserToLocalStorage(currentUser);
       nav(`/user/${currentUser.name}`);
     } catch (err) {
@@ -87,108 +99,18 @@ const Register: React.FC = () => {
   };
 
   return (
-    <>
-      <Container maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            mt: 4,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            p: 3,
-            boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
-            border: "2px solid #E5D6F2",
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "#F5B3D7" }}>
-            <LockOutlined />
-          </Avatar>
-          <Typography variant="h5">Register</Typography>
-          <Box
-            sx={{
-              mt: 3,
-            }}
-          >
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  name="name"
-                  required
-                  fullWidth
-                  id="name"
-                  label="Name"
-                  autoFocus
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  error={incorrectEmailFormat}
-                  helperText={incorrectEmailFormat && "Please enter a valid email address"}
-                  inputProps={{
-                    pattern: "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$",
-                    title: "Please enter a valid email address",
-                  }}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  error={incorrectPasswordFormat}
-                  helperText={incorrectPasswordFormat && "Password must be at least 8 characters long"}
-                  inputProps={{
-                    minLength: 8,
-                    title: "Password must be at least 8 characters long",
-                  }}
-                />
-              </Grid>
-            </Grid>
-            {emailAlreadyRegistered && (
-              <Typography sx={{ color: "red", fontSize: 12 }}>
-                Email already registered
-              </Typography>
-            )}
-            <Button
-              fullWidth
-              variant="contained"
-              sx={{
-                mt: 3,
-                mb: 2,
-                backgroundColor: "#EDC2F0",
-                "&:hover": {
-                  backgroundColor: "#EDC2F0",
-                },
-              }}
-              onClick={handleRegister}
-            >
-              Register
-            </Button>
-            <Grid container justifyContent="center">
-              <Grid item>Already have an account?</Grid>
-              <Grid item>
-                <Link to="/login">Login</Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-      </Container>
-    </>
+    <RegisterDisplay
+      name={name}
+      setName={setName}
+      email={email}
+      setEmail={setEmail}
+      password={password}
+      setPassword={setPassword}
+      incorrectEmailFormat={incorrectEmailFormat}
+      incorrectPasswordFormat={incorrectPasswordFormat}
+      emailAlreadyRegistered={emailAlreadyRegistered}
+      handleRegister={handleRegister}
+    />
   );
 };
 
