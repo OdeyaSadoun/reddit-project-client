98c581551de83b1858ff533d1cf94917ad438c8d
אודיה סעדון <osadoun@g.jct.ac.il>
2024-02-21 07:52:41 +0200

    add login display comps and interdface for his props

---
+++ frontend/src/components/display/auth/LoginDisplay.tsx
@@ -0,0 +1,103 @@
+import { Link } from "react-router-dom";
+import { LoginDisplayProps } from "../../../interfaces/LoginDisplayProps.interface";
+
+import { LockOutlined } from "@mui/icons-material";
+import {
+  Container,
+  CssBaseline,
+  Box,
+  Avatar,
+  Typography,
+  TextField,
+  Button,
+  Grid,
+} from "@mui/material";
+
+
+const LoginDisplay: React.FC<LoginDisplayProps> = ({
+  email,
+  setEmail,
+  password,
+  setPassword,
+  handleLogin,
+  incorrectUserOrPassword,
+}) => {
+  return (
+    <>
+      <Container maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            mt: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            p: 3,
+            boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
+            border: "2px solid #E5D6F2",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "#F5B3D7" }}>
+            <LockOutlined />
+          </Avatar>
+          <Typography variant="h5">Login</Typography>
+          <Box sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoFocus
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="password"
+              name="password"
+              label="Password"
+              type="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+            {incorrectUserOrPassword && (
+              <Typography sx={{ color: "red", fontSize: 12 }}>
+                Incorrect email or password
+              </Typography>
+            )}
+            <Button
+              fullWidth
+              variant="contained"
+              sx={{
+                mt: 3,
+                mb: 2,
+                backgroundColor: "#EDC2F0",
+                "&:hover": {
+                  backgroundColor: "#EDC2F0",
+                },
+              }}
+              onClick={handleLogin}
+            >
+              Login
+            </Button>
+
+            <Grid container justifyContent={"center"}>
+              <Grid item>Don't have an account? </Grid>
+              <Grid item>
+                <Link to="/register">Register</Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </>
+  );
+};
+
+export default LoginDisplay;
---
+++ frontend/src/interfaces/LoginDisplayProps.interface.ts
@@ -0,0 +1,8 @@
+export interface LoginDisplayProps {
+  email: string;
+  setEmail: React.Dispatch<React.SetStateAction<string>>;
+  password: string;
+  setPassword: React.Dispatch<React.SetStateAction<string>>;
+  handleLogin: () => void;
+  incorrectUserOrPassword: boolean;
+}
